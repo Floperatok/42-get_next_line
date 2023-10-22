adecd8b4262755f50d17bebc743401ae75711d79
Floperatok <floperatox@gmail.com>
2023-10-22 12:16:39 +0200

    mandatory part, init commit

---
+++ get_next_line_utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsalles <nsalles@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 13:21:14 by nsalles           #+#    #+#             */
+/*   Updated: 2023/10/22 11:45:36 by nsalles          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *s)
+{
+	char	*d;
+	int		i;
+
+	d = malloc(ft_strlen(s) * sizeof(char) + 1);
+	if (d == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		d[i] = s[i];
+		i++;
+	}
+	d[i] = '\0';
+	return (d);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*res;
+	size_t	lens1;
+	size_t	lens2;
+	size_t	i;
+
+	lens1 = ft_strlen(s1);
+	lens2 = ft_strlen(s2);
+	res = (char *)malloc(sizeof(char) * (lens1 + lens2 + 1));
+	if (!res)
+	{
+		free(s1);
+		return (NULL);
+	}
+	i = -1;
+	while (++i < lens1)
+		res[i] = s1[i];
+	i--;
+	while (++i < lens1 + lens2)
+		res[i] = s2[i - lens1];
+	res[i] = '\0';
+	free(s1);
+	return (res);
+}
